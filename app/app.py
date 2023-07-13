349a08d08bb0ff3a55d140e291e1406140e824b6
aldo.luria <aldo.luria@gmail.com>
2023-07-12 23:45:36 -0600

    Login encriptado

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import os
+import pdfkit
 from flask import Flask, render_template, request, url_for, redirect, flash, jsonify, Blueprint, make_response
 from flask_mysqldb import MySQL
 from flask_wtf.csrf import CSRFProtect
 from datetime import datetime
 from werkzeug.utils import secure_filename
-import pdfkit
+from werkzeug.security import generate_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required
+
+from Models.ModelUser import ModuleUser
+from Models.entities.user import User
 #importamos Blueprint para crear una etiqueta
 
 #Creamos una tag con la ayuda de Blueprint y la iniciamos en nuestro proyecti (al crear nuestra applicación)
@@ -24,6 +29,18 @@ ruta=app.config['UPLOAD_FOLDER']='./app/static/img/uploads/profesores'
 
 db = MySQL(app)
 
+Login_manager_app=LoginManager(app)
+
+@Login_manager_app.user_loader
+def load_user(idusuarios):
+    return ModuleUser.get_by_id(db,idusuarios)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template('login.html')
+
+
 app.secret_key='mysecretkey'
 
 #Usamos el nuevo tag que creamos y le asignamos una función
@@ -36,6 +53,7 @@ def listar_materias():
     cur.close()
     return materias
 
+@login_required
 def listar_profesores():
     cur=db.connection.cursor()
     sql="SELECT * FROM profesores order by nombre asc"
@@ -65,6 +83,7 @@ def index():
 # ---------------------------------------------- Apartado CRUD de Alumnos ---------------------------------------------
 #Read (Leer)
 @app.route('/alumnos')
+@login_required
 def alumnos_Ver():
     cur=db.connection.cursor()
     sql="SELECT * FROM alumnos order by matricula asc"
@@ -76,6 +95,7 @@ def alumnos_Ver():
     return render_template('alumnos.html', alumnos=alumnos)
 
 @app.route('/alumno/<string:id>')
+@login_required
 def ver_alumno(id):
     print(id)
     cur=db.connection.cursor()
@@ -89,6 +109,7 @@ def ver_alumno(id):
 
 #Generar PDF de Alumno en otra ventana
 @app.route('/alumno/<string:id>/PDF')
+@login_required
 def ver_alumno_PDF(id):
     print(id)
     cur=db.connection.cursor()
@@ -122,10 +143,12 @@ def ver_alumno_PDF(id):
 
 #Create (Crear)
 @app.route('/alumno/nuevo')
+@login_required
 def alumno_Crear():
     return render_template('alumnoCrear.html',)
 
 @app.route('/alumno/agregar', methods=['POST'])
+@login_required
 def alumno_Agregar():
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -145,6 +168,7 @@ def alumno_Agregar():
 
 #Update (Actualizar)
 @app.route('/alumno/editar/<string:id>')
+@login_required
 def editar_alumno(id):
     print(id)
     cur=db.connection.cursor()
@@ -156,6 +180,7 @@ def editar_alumno(id):
     return render_template('alumnoEditar.html', alumno=alumno[0])
 
 @app.route('/alumno/actualizar/<string:id>', methods=['POST'])
+@login_required
 def alumno_actualizar(id):
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -173,6 +198,7 @@ def alumno_actualizar(id):
 
 #Delete (Borrar)
 @app.route('/alumno/eliminar/<string:id>')
+@login_required
 def eliminar_alumno(id):
     print(id)
     cur=db.connection.cursor()
@@ -187,6 +213,7 @@ def eliminar_alumno(id):
 # ---------------------------------------------- Apartado CRUD de Profesores ----------------------------------------------
 #Read (Leer)
 @app.route('/profesores')
+@login_required
 def profesores_Ver():
     cur=db.connection.cursor()
     sql="SELECT * FROM profesores ORDER BY idprofesores desc"
@@ -197,6 +224,7 @@ def profesores_Ver():
     return render_template('profesores.html', profesores=profesores)
 
 @app.route('/profesor/<string:id>')
+@login_required
 def ver_profesor(id):
     print(id)
     cur=db.connection.cursor()
@@ -209,10 +237,12 @@ def ver_profesor(id):
 
 #Create (Crear)
 @app.route('/profesor/nuevo')
+@login_required
 def profesor_Crear():
     return render_template('profesorCrear.html',)
 
 @app.route('/profesor/agregar', methods=['POST'])
+@login_required
 def profesor_Agregar():
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -250,6 +280,7 @@ def profesor_Agregar():
 
 #Update (Actualizar)
 @app.route('/profesor/editar/<string:id>')
+@login_required
 def editar_profesor(id):
     print(id)
     cur=db.connection.cursor()
@@ -261,6 +292,7 @@ def editar_profesor(id):
     return render_template('profesorEditar.html', profesor=profesor[0])
 
 @app.route('/profesor/actualizar/<string:id>', methods=['POST'])
+@login_required
 def profesor_actualizar(id):
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -277,6 +309,7 @@ def profesor_actualizar(id):
 
 #Delete (Borrar)
 @app.route('/profesor/eliminar/<string:id>')
+@login_required
 def eliminar_profesor(id):
     print(id)
     cur=db.connection.cursor()
@@ -290,6 +323,7 @@ def eliminar_profesor(id):
 # ---------------------------------------------- Apartado CRUD de Materias ----------------------------------------------
 #Read (Leer)
 @app.route('/materias')
+@login_required
 def materias_Ver():
     cur=db.connection.cursor()
     #sql="SELECT m.idmaterias, m.nombre, m.idprofesor, p.numero_empleado, p.nombre FROM materias as m INNER JOIN profesores as p ON m.idprofesor = p.idprofesores ORDER BY idmaterias desc"
@@ -301,6 +335,7 @@ def materias_Ver():
     return render_template('materias.html', materias=materias)
 
 @app.route('/materia/<string:id>')
+@login_required
 def ver_materia(id):
     print(id)
     cur=db.connection.cursor()
@@ -313,10 +348,12 @@ def ver_materia(id):
 
 #Create (Crear)
 @app.route('/materia/nuevo')
+@login_required
 def materia_Crear():
     return render_template('materiaCrear.html',profesores=listar_profesores())
 
 @app.route('/materia/agregar', methods=['POST'])
+@login_required
 def materia_Agregar():
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -335,6 +372,7 @@ def materia_Agregar():
 
 #Update (Actualizar)
 @app.route('/materia/editar/<string:id>')
+@login_required
 def editar_materia(id):
     print(id)
     cur=db.connection.cursor()
@@ -346,6 +384,7 @@ def editar_materia(id):
     return render_template('materiaEditar.html', materia=materia[0])
 
 @app.route('/materia/actualizar/<string:id>', methods=['POST'])
+@login_required
 def materia_actualizar(id):
     if request.method == 'POST':
         Nombre=request.form['Nombre']
@@ -363,6 +402,7 @@ def materia_actualizar(id):
 
 #Delete (Borrar)
 @app.route('/materia/eliminar/<string:id>')
+@login_required
 def eliminar_materia(id):
     print(id)
     cur=db.connection.cursor()
@@ -374,12 +414,149 @@ def eliminar_materia(id):
     return redirect(url_for('materias_Ver'))
 
 
+# ---------------------------------------------- Apartado CRUD de Usuarios ----------------------------------------------
+#Read (Leer)
+@app.route('/usuarios')
+@login_required
+def usuarios_Ver():
+    cur=db.connection.cursor()
+    sql="SELECT * FROM usuarios ORDER BY idusuarios desc"
+    cur.execute(sql)
+    usuarios=cur.fetchall()
+    print(usuarios) 
+    cur.close()
+    return render_template('usuarios.html', usuarios=usuarios)
+
+@app.route('/usuario/<string:id>')
+@login_required
+def ver_usuario(id):
+    print(id)
+    cur=db.connection.cursor()
+    sql="SELECT * FROM usuarios WHERE idusuarios={0}".format(id)
+    cur.execute(sql)
+    usuario=cur.fetchall()
+    print(usuario[0])
+    cur.close()
+    return render_template('usuarioVer.html', usuario=usuario[0])
+
+#Create (Crear)
+@app.route('/usuario/nuevo')
+@login_required
+def usuario_Crear():
+    return render_template('usuarioCrear.html',)
+
+@app.route('/usuario/agregar', methods=['POST'])
+@login_required
+def usuario_Agregar():
+    if request.method == 'POST':
+        Username=request.form['Username']
+        Password=request.form['Password']
+        Pass=generate_password_hash(Password)
+        TipoUsuario=request.form['TipoUsuario']
+        Creado=datetime.now()
+        Activo=1
+
+        cur=db.connection.cursor()
+        sql="INSERT INTO usuarios (username, password, tipo, creado, activo) VALUES (%s, %s, %s,%s, %s)"
+        valores=(Username, Pass, TipoUsuario, Creado, Activo)
+        cur.execute(sql,valores)
+        db.connection.commit()
+        cur.close()
+
+        flash('¡Usuario agregado exitosamente!')
+
+        return redirect(url_for('usuarios_Ver'))
+
+#Update (Actualizar)
+@app.route('/usuario/editar/<string:id>')
+@login_required
+def editar_usuario(id):
+    print(id)
+    cur=db.connection.cursor()
+    sql="SELECT * FROM usuarios WHERE idusuarios={0}".format(id)
+    cur.execute(sql)
+    usuario=cur.fetchall()
+    print(usuario[0])
+    cur.close()
+    return render_template('usuarioEditar.html', usuario=usuario[0])
+
+@app.route('/usuario/actualizar/<string:id>', methods=['POST'])
+@login_required
+def usuario_actualizar(id):
+    if request.method == 'POST':
+        Username=request.form['Username']
+        Password=request.form['Password']
+        Pass=generate_password_hash(Password)
+        TipoUsuario=request.form['TipoUsuario']
+        Activo=1
+
+        cur=db.connection.cursor()
+        sql="UPDATE usuarios SET username=%s, password=%s, tipo=%s, activo=%s WHERE idusuarios=%s"        
+        valores=(Username, Pass, TipoUsuario, Activo, id)
+        cur.execute(sql,valores)
+        db.connection.commit()
+        cur.close()
+
+        flash('¡Usuario modificado exitosamente!')
+    return redirect(url_for('usuarios_Ver'))
+
+#Delete (Borrar)
+@app.route('/usuario/eliminar/<string:id>')
+@login_required
+def eliminar_usuario(id):
+    print(id)
+    cur=db.connection.cursor()
+    sql="DELETE FROM usuarios WHERE idusuarios={0}".format(id)
+    cur.execute(sql)
+    db.connection.commit()
+    cur.close()
+    flash('¡Usuario eliminado correctamente!')
+    return redirect(url_for('usuarios_Ver'))
+
+### Apartado SingUp
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+### Apartado Login
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/loguear', methods=['POST'])
+def loguear():
+    if request.method == 'POST':
+        Username=request.form['Username']
+        Password=request.form['Password']
+        user=User(0,Username,Password,None)
+        loged_user=ModuleUser.login(db,user)
+
+        if loged_user!= None:
+            if loged_user.password:
+                login_user(loged_user)
+                return redirect(url_for('usuarios_Ver'))
+            else:
+                flash('Nombre de usuario y/o Contraseña incorrecta.')
+                return render_template('login.html')
+        else:
+            flash('Nombre de usuario y/o Contraseña incorrecta.')
+            return render_template('login.html')
+    else:
+            flash('Nombre de usuario y/o Contraseña incorrecta.')
+            return render_template('login.html')
+
+
 def pagina_no_encontrada(error):
     #return render_template('404.html'), 404
     return redirect(url_for('index'))
+def acceso_no_autorizado(error):
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     csrf.init_app(app)
     app.register_blueprint(custom_tags)
     app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(401, acceso_no_autorizado)
     app.run(debug=True, port=5000)
